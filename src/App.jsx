735bafc3b1b3b7ed7807ac587d6c0d18a0a4bb1d
Bilal <bilalmuhammadyousuf543@gmail.com>
2023-10-28 23:55:10 +0500

    fetching ipv6 address

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,29 @@ import Banner from './Component/Banner'
 import Exhibit from './Component/Exhibit'
 
 function App() {
-  const [details, setDetails] = useState(null)
+  const [IPV4Details, setIPV4Details] = useState(null)
 
-  const getUserDetails = () => {
-    fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data => setDetails(data))
+  const getUserIPV4ddress = () => {
+    fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data => setIPV4Details(data))
+  }
+
+  const [IPV6Details, setIPV6Details] = useState(null)
+
+  const getUserIPV6ddress = () => {
+    fetch('https://api64.ipify.org?format=json').then(response => response.json()).then(data => setIPV6Details(data))
   }
   return (
     <>
-      <p>hello world</p>
-      <Banner bannerName="Sextant" />
+      <Banner bannerName="Sextant Dashboard" />
       <Exhibit text="I am from exhibit component" />
-      <button onClick={getUserDetails}>Find My Details</button>
+      <button onClick={getUserIPV4ddress}>Find IPV4 Address</button>
       {
-        details && <p>Location: {`${details.ip}`}</p>
+        IPV4Details && <p>Location: {`${IPV4Details.ip}`}</p>
+      }
+      <br></br>
+      <button onClick={getUserIPV6ddress}>Find IPV6 Address</button>
+      {
+        IPV6Details && <p>Location: {`${IPV6Details.ip}`}</p>
       }
     </>
   )
