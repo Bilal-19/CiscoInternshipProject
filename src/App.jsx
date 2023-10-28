8a19546d6ea815dcd0de7dc944acfd2d89509c49
Bilal <bilalmuhammadyousuf543@gmail.com>
2023-10-28 23:44:17 +0500

    fetching ipv4 address

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react'
 import './App.css'
+import Banner from './Component/Banner'
+import Exhibit from './Component/Exhibit'
 
 function App() {
+  const [details, setDetails] = useState(null)
+
+  const getUserDetails = () => {
+    fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data => setDetails(data))
+  }
   return (
     <>
       <p>hello world</p>
-      <div className="container"></div>
+      <Banner bannerName="Sextant" />
+      <Exhibit text="I am from exhibit component" />
+      <button onClick={getUserDetails}>Find My Details</button>
+      {
+        details && <p>Location: {`${details.ip}`}</p>
+      }
     </>
   )
 }
